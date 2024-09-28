80c51f802eefb377ff2b2daf36d78b1bfcfcf18c
Gustavo Viana <gusvianadev@gmail.com>
2024-09-28 18:53:38 -0300

    ejercicios 1 al 6

--- Solucion.hs
+++ Solucion.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use null" #-}
 module Solucion where
 
 -- Completar!
@@ -14,6 +17,9 @@ type Vuelo = (Ciudad, Ciudad, Duracion)
 
 type AgenciaDeViajes = [Vuelo]
 
+vueloNulo :: Vuelo
+vueloNulo = ("", "", 0.0)
+
 -- EJERCICIO 1
 vuelosValidos :: AgenciaDeViajes -> Bool
 vuelosValidos [] = True
@@ -24,7 +30,7 @@ vuelosValidos (vuelo : resto) =
     && vuelosValidos resto
 
 vueloEsRepetido :: Vuelo -> AgenciaDeViajes -> Bool
-vueloEsRepetido vuelo [] = False
+vueloEsRepetido _ [] = False
 vueloEsRepetido vuelo [ultimo] = compararVuelos vuelo ultimo
 vueloEsRepetido vuelo (primero : resto) =
   compararVuelos vuelo primero || vueloEsRepetido vuelo resto
@@ -38,24 +44,144 @@ vueloValido (origen, destino, duracion) = duracion > 0 && origen /= destino
 
 -- EJERCICIO 2
 ciudadesConectadas :: AgenciaDeViajes -> Ciudad -> [Ciudad]
-ciudadesConectadas _ _ = ["BsAs"] -- Borrar y escribir el código correcto
+ciudadesConectadas [] _ = []
+ciudadesConectadas vuelos ciudad = eliminarDuplicados (resolver vuelos ciudad)
+  where
+    resolver :: AgenciaDeViajes -> Ciudad -> [Ciudad]
+    resolver [] _ = []
+    resolver ((origen, destino, _) : resto) ciudad
+      | ciudad == origen = destino : resolver resto ciudad
+      | ciudad == destino = origen : resolver resto ciudad
+      | otherwise = resolver resto ciudad
 
 -- EJERCICIO 3
 modernizarFlota :: AgenciaDeViajes -> AgenciaDeViajes
-modernizarFlota _ = [("BsAs", "Rosario", 9.0)] -- Borrar y escribir el código correcto
+modernizarFlota [] = []
+modernizarFlota ((origen, destino, duracion) : resto) =
+  (origen, destino, duracion * 0.9) : modernizarFlota resto
 
 -- EJERCICIO 4
 ciudadMasConectada :: AgenciaDeViajes -> Ciudad
-ciudadMasConectada _ = "Rosario" -- Borrar y escribir el código correcto
+ciudadMasConectada [] = []
+ciudadMasConectada vuelos = masRepetido (resolver vuelos)
+  where
+    resolver :: AgenciaDeViajes -> [Ciudad]
+    resolver [] = []
+    resolver ((origen, destino, _) : resto) = origen : destino : resolver resto
 
 -- EJERCICIO 5
 sePuedeLlegar :: AgenciaDeViajes -> Ciudad -> Ciudad -> Bool
-sePuedeLlegar vuelos origen destino = True -- Borrar y escribir el código correcto
+sePuedeLlegar [] _ _ = False
+sePuedeLlegar vuelos origen destino =
+  buscarSinEscala vuelos origen destino /= vueloNulo
+    || length (buscarConEscala vuelos origen destino) > 0
+
+buscarSinEscala :: AgenciaDeViajes -> Ciudad -> Ciudad -> Vuelo
+buscarSinEscala [] _ _ = ("", "", 0)
+buscarSinEscala (vuelo : resto) origen destino
+  | origen == principio && final == destino = vuelo
+  | otherwise = buscarSinEscala resto origen destino
+  where
+    (principio, final, duracion) = vuelo
+
+buscarConEscala :: AgenciaDeViajes -> Ciudad -> Ciudad -> AgenciaDeViajes
+buscarConEscala [] _ _ = []
+buscarConEscala (vuelo : resto) origen destino
+  | origen == principio && vueloDestinoFinalSinEscala /= vueloNulo =
+      vuelo
+        : vueloDestinoFinalSinEscala
+        : buscarConEscala resto origen destino
+  | destino == final -- Jeje
+      && vueloOrigenPrincipioSinEscala /= vueloNulo =
+      vuelo : vueloOrigenPrincipioSinEscala : buscarConEscala resto origen destino
+  | otherwise = buscarConEscala resto origen destino
+  where
+    (principio, final, duracion) = vuelo
+    vueloDestinoFinalSinEscala = buscarSinEscala resto final destino -- vuelo 180 go boom
+    vueloOrigenPrincipioSinEscala = buscarSinEscala resto origen principio
 
 -- EJERCICIO 6
 duracionDelCaminoMasRapido :: AgenciaDeViajes -> Ciudad -> Ciudad -> Duracion
-duracionDelCaminoMasRapido _ _ _ = 10.0 -- Borrar y escribir el código correcto
+duracionDelCaminoMasRapido vuelos origen destino = minimoEnDuraciones duraciones
+  where
+    duraciones = sinEscala : duracionesConEscala conEscala
+    (_, _, sinEscala) = buscarSinEscala vuelos origen destino
+    conEscala = buscarConEscala vuelos origen destino
+    duracionesConEscala :: AgenciaDeViajes -> [Duracion]
+    duracionesConEscala [] = []
+    duracionesConEscala ((_, _, duracion1) : (_, _, duracion2) : resto) =
+      (duracion1 + duracion2) : duracionesConEscala resto
+
+minimo :: Float -> Float -> Float
+minimo primero segundo
+  | primero < segundo = primero
+  | otherwise = segundo
+
+minimoEnDuraciones :: [Float] -> Float
+minimoEnDuraciones [duracion] = duracion
+minimoEnDuraciones (primero : segundo : resto) =
+  minimo primero minimoDeSegundo
+  where
+    minimoDeSegundo = minimoEnDuraciones (segundo : resto)
 
 -- EJERCICIO 7
 puedoVolverAOrigen :: AgenciaDeViajes -> Ciudad -> Bool
-puedoVolverAOrigen vuelos origen = True -- Borrar y escribir el código correcto
+puedoVolverAOrigen vuelos origen =
+  buscarConMultiplesEscalas vuelos origen || puedoVolverAOrigen (tail vuelos) origen
+
+buscarIdaYVueltaSinEscala :: AgenciaDeViajes -> Ciudad -> Bool
+buscarIdaYVueltaSinEscala [] _ = False
+buscarIdaYVueltaSinEscala ((principio, final, _) : resto) origen
+  | principio == origen = buscarSinEscala resto final origen /= vueloNulo
+  | otherwise = buscarIdaYVueltaSinEscala resto origen
+
+-- buscarIdaYVueltaConEscala :: AgenciaDeViajes -> Ciudad -> Bool
+-- buscarIdaYVueltaConEscala [] _ = False
+-- buscarIdaYVueltaConEscala ((principio, final, _) : resto) origen
+--   | principio == origen = buscarConEscala resto final origen /= vueloNulo
+--   | otherwise = buscarIdaYVueltaConEscala resto origen
+
+buscarConMultiplesEscalas :: AgenciaDeViajes -> Ciudad -> Bool
+buscarConMultiplesEscalas ((principio, final, _) : resto) origen
+  | principio == origen = sinEscala /= vueloNulo || conEscala
+  | otherwise = buscarConMultiplesEscalas resto origen
+  where
+    sinEscala = buscarSinEscala resto final origen
+    conEscala = buscarIdaYVueltaConMultiplesEscalas resto origen
+
+buscarIdaYVueltaConMultiplesEscalas :: AgenciaDeViajes -> Ciudad -> Bool
+buscarIdaYVueltaConMultiplesEscalas ((principio, final, _) : resto) origen = resolver
+  where
+    resolver :: Bool
+    resolver = buscarSinEscala resto final origen /= vueloNulo
+
+-- Auxiliares
+
+eliminarDuplicados :: (Eq t) => [t] -> [t]
+eliminarDuplicados [] = []
+eliminarDuplicados (x : xs) = x : eliminarDuplicados (eliminarOcurrencias x xs)
+
+eliminarOcurrencias :: (Eq t) => t -> [t] -> [t]
+eliminarOcurrencias a [] = []
+eliminarOcurrencias a (b : rest)
+  | a == b = eliminarOcurrencias a rest
+  | otherwise = b : eliminarOcurrencias a rest
+
+masRepetido :: (Eq t) => [t] -> t
+masRepetido [a] = a
+masRepetido lista = resolver lista lista
+  where
+    resolver :: (Eq t) => [t] -> [t] -> t
+    resolver lista [a, b]
+      | cantidadApariciones a lista > cantidadApariciones b lista = a
+      | otherwise = b
+    resolver lista (a : b : rest)
+      | cantidadApariciones a lista > cantidadApariciones b lista =
+          resolver lista (a : rest)
+      | otherwise = resolver lista (b : rest)
+
+cantidadApariciones :: (Eq t) => t -> [t] -> Int
+cantidadApariciones _ [] = 0
+cantidadApariciones a (b : rest)
+  | a == b = 1 + cantidadApariciones a rest
+  | otherwise = cantidadApariciones a rest
--- test-propios.hs
+++ test-propios.hs
@@ -7,7 +7,10 @@ runAllTests = runTestTT allTests
 allTests =
   test
     [ "vuelosValidos" ~: testVuelosValidos,
-      "ciudadesConectadas" ~: testCiudadesConectadas
+      "ciudadesConectadas" ~: testCiudadesConectadas,
+      "modernizarFlota" ~: testModernizarFlota,
+      "ciudadMasConectada" ~: testCiudadMasConectada,
+      "sePuedeLlegar" ~: testSePuedeLLegar
     ]
 
 testVuelosValidos =
@@ -80,4 +83,147 @@ testCiudadesConectadas =
           ]
           "BsAs"
         ~?= ["Rosario", "La Quiaca"]
+    ]
+
+testModernizarFlota =
+  test
+    [ "modernizar flota sin vuelos" ~: modernizarFlota [] ~?= [],
+      "modernizar flota con un vuelo"
+        ~: modernizarFlota [("BsAs", "Rosario", 10.0)]
+        ~?= [("BsAs", "Rosario", 9.0)],
+      "modernizar flota con dos vuelos"
+        ~: modernizarFlota [("BsAs", "Rosario", 57.0), ("La Plata", "Ciudad Chistosa", 20.0)]
+        ~?= [("BsAs", "Rosario", 51.3), ("La Plata", "Ciudad Chistosa", 18.0)],
+      "modernizar flota con tres vuelos"
+        ~: modernizarFlota
+          [ ("BsAs", "Rosario", 57.0),
+            ("La Plata", "Ciudad Chistosa", 20.0),
+            ("La Pampa", "La Rioja", 4.0)
+          ]
+        ~?= [ ("BsAs", "Rosario", 51.3),
+              ("La Plata", "Ciudad Chistosa", 18.0),
+              ("La Pampa", "La Rioja", 3.6)
+            ]
+    ]
+
+testCiudadMasConectada =
+  test
+    [ "ciudad mas conectada sin vuelos" ~: ciudadMasConectada [] ~?= [],
+      "ciudad mas conectada con un vuelo"
+        ~: ciudadMasConectada [("BsAs", "Rosario", 10.0)]
+        ~?= "Rosario",
+      "ciudad mas conectada con dos vuelos"
+        ~: ciudadMasConectada [("BsAs", "Rosario", 57.0), ("BsAs", "Ciudad Chistosa", 20.0)]
+        ~?= "BsAs",
+      "ciudad mas conectada con tres vuelos"
+        ~: ciudadMasConectada
+          [ ("BsAs", "Rosario", 57.0),
+            ("La Plata", "Ciudad Chistosa", 20.0),
+            ("La Pampa", "La Plata", 4.5)
+          ]
+        ~?= "La Plata"
+    ]
+
+origen :: String
+origen = "BsAs"
+
+destino :: String
+destino = "Tucuman"
+
+testSePuedeLLegar =
+  test
+    [ "no se puede llegar sin vuelos" ~: sePuedeLlegar [] origen destino ~?= False,
+      "se puede llegar con un vuelo que matchea"
+        ~: sePuedeLlegar [("BsAs", "Tucuman", 10.0)] origen destino
+        ~?= True,
+      "no se puede llegar con un vuelo que no matchea"
+        ~: sePuedeLlegar [("BsAs", "Rosario", 10.0)] origen destino
+        ~?= False,
+      "se puede llegar sin escala con lista con multiples vuelos"
+        ~: sePuedeLlegar
+          [ ("BsAs", "Rosario", 10.0),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "Tucuman", 10.0),
+            ("Rosario", "Tucuman", 10.0)
+          ]
+          origen
+          destino
+        ~?= True,
+      "se puede llegar con maximo una escala con multiples vuelos, encontrando primero el origen"
+        ~: sePuedeLlegar
+          [ ("BsAs", "Rosario", 10.0),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "La Plata", 10.0),
+            ("Konoha", "Ciudad Chistosa", 10.0),
+            ("Rosario", "Brasil", 10.0),
+            ("Rosario", "Tucuman", 10.0),
+            ("Rosario", "Bulnes", 10.0)
+          ]
+          origen
+          destino
+        ~?= True,
+      "se puede llegar con maximo una escala con multiples vuelos, encontrando primero el destino"
+        ~: sePuedeLlegar
+          [ ("Rosario", "Tucuman", 10.0),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "La Plata", 10.0),
+            ("Konoha", "Ciudad Chistosa", 10.0),
+            ("Rosario", "Brasil", 10.0),
+            ("BsAs", "Rosario", 10.0),
+            ("Rosario", "Bulnes", 10.0)
+          ]
+          origen
+          destino
+        ~?= True
+    ]
+
+testDuracionMasCorta =
+  test
+    [ "duracion del camino mas rapido con un vuelo"
+        ~: duracionDelCaminoMasRapido [("BsAs", "Tucuman", 10.0)] origen destino
+        ~?= 10.0,
+      "duracion del camino mas rapido con un vuelo directo y uno con escala, donde directo es mas rapido"
+        ~: duracionDelCaminoMasRapido
+          [ ("BsAs", "Tucuman", 3.4),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "Rosario", 4.0),
+            ("Rosario", "Tucuman", 3.3)
+          ]
+          origen
+          destino
+        ~?= 3.4,
+      "duracion del camino mas rapido con un vuelo directo y uno con escala, donde escala es mas rapido"
+        ~: duracionDelCaminoMasRapido
+          [ ("BsAs", "Tucuman", 9.8),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "Rosario", 4.0),
+            ("Rosario", "Tucuman", 3.3)
+          ]
+          origen
+          destino
+        ~?= 7.3,
+      "duracion del camino mas rapido con un vuelo directo y dos con escala, donde directo es mas rapido"
+        ~: duracionDelCaminoMasRapido
+          [ ("BsAs", "Tucuman", 4.8),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "Rosario", 4.0),
+            ("Rosario", "Tucuman", 3.3),
+            ("BsAs", "La Plata", 2.0),
+            ("La Plata", "Tucuman", 3.3)
+          ]
+          origen
+          destino
+        ~?= 4.8,
+      "duracion del camino mas rapido con un vuelo directo y dos con escala, donde escala es mas rapido"
+        ~: duracionDelCaminoMasRapido
+          [ ("BsAs", "Tucuman", 9.8),
+            ("Cordoba", "Tucuman", 10.0),
+            ("BsAs", "Rosario", 4.0),
+            ("Rosario", "Tucuman", 3.3),
+            ("BsAs", "La Plata", 2.0),
+            ("La Plata", "Tucuman", 3.3)
+          ]
+          origen
+          destino
+        ~?= 5.3
     ]
